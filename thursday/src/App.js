38f64999d0760a3309eb14f3ff324b74bd226548
Sneha Choudhary <snehac@missionreadyhq.com>
2022-09-23 08:14:02 +0530

    changes

---
+++ thursday/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ background: 'green' }}>
+      <h1>Hello People</h1>
+      <h2>test2</h2>
+      <h2>test3</h2>
+      <h2>test4</h2>
+      <p>This is a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
